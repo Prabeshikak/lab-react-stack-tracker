b41958af3421696d06c056f69c1f39a79af5df37
Prabeshika <prabeshika14@gmail.com>
2024-03-07 12:45:54 +0100

    react-stack-tracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import { Routes, Route } from "react-router-dom";
+import CompanyPage from "./pages/CompanyPage";
+import TechnologyPage from "./pages/TechnologyPage";
+import companiesData from "./companies.json";
 
 function App() {
+  const [companies, setCompanies] = useState(companiesData);
+  const [technologiesData, setTechnologiesData] = useState([]);
   return (
     <div className="App">
-      <h1>LAB | React Stack Tracker</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage companies={companies} />} />
+        <Route
+          path="/company/:companySlug"
+          element={<CompanyPage companies={companies} />}
+        />
+        <Route path="/tech/:slug" element={<TechnologyPage />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/CompanyPage.jsx
+++ src/pages/CompanyPage.jsx
@@ -1,7 +1,21 @@
-function CompanyPage() {
+import { useParams } from "react-router-dom";
+
+function CompanyPage(props) {
+  const { companyId } = useParams();
+  const foundCompany = props.companies.find((oneCompany) => {
+    return oneCompany._id === String(companyId);
+  });
   return (
     <div>
-      <h1>CompanyPage</h1>
+      <h1>Company Profile</h1>
+      {!foundCompany && <h3>Company not found!</h3>}
+      {foundCompany && (
+        <>
+          <p>{foundCompany.name}</p>
+          <p>{foundCompany.description}</p>
+          <p>{foundCompany.website}</p>
+        </>
+      )}
     </div>
   );
 }
